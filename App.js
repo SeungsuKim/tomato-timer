bb0d0863436f98d2302bbd409425beebfe3a9680
seungsukim <seungsu0407@gmail.com>
2019-04-30 10:44:42 +0900

    ADD: Full functions

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import Timer from './components/Timer';
+import reducer from "./reducer";
+
+let store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+      <Provider store={store}>
+        <Timer />
+      </Provider>
+    )
   }
 }
 
